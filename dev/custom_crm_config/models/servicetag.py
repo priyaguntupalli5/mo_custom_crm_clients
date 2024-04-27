23e651c27cae5826b97a8449ff643e6d06f202ca
priyaguntupalli5 <guntupallisaipriya@gmail.com>
2024-04-27 11:48:07 -0300

    fixed service tag model

--- dev/custom_crm_config/models/servicetag.py
+++ dev/custom_crm_config/models/servicetag.py
@@ -2,7 +2,8 @@ from odoo import models, fields, api, _
 
 
 class ServiceTag(models.Model):
-    _name = 'crm.servicetag'
+    _name = 'crm.service.tag'
     _description = "CRM Services Tags"
+    name = fields.Char(string="Name", required=True)
+    color = fields.Integer(string="Color", required=True)
 
-    tag = fields.Char(string='tag', required = True)
