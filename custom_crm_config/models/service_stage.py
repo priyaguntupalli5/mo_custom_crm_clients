e4b18e8f8319bcc9a7304fd9bdcf15d83fe78cb2
priyaguntupalli5 <guntupallisaipriya@gmail.com>
2024-04-27 16:45:36 -0300

    fixed stage model

--- custom_crm_config/models/service_stage.py
+++ custom_crm_config/models/service_stage.py
@@ -4,10 +4,7 @@ from odoo import models, fields, api, _
 class ServiceStage(models.Model):
     _name = 'crm.service.stage'
     _description = "CRM Services Stages"
-
-    New = fields.Char(string='New', required = True)
-    in_progress = fields.Char(string='In Progress', required = True)
-    delayed = fields.Char(string='Delayed', required = True)
-    done = fields.Char(string='Done', required = True)
+    stage_seq = fields.Integer(string='stage sequence')
+    stage_name = fields.Char(string='Stage Name', required=True, translate=True)
 
 
