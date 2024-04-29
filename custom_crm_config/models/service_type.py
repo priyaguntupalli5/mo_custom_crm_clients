8d279561b350cb5904d482d217db04d3311f17cc
priyaguntupalli5 <guntupallisaipriya@gmail.com>
2024-04-29 17:49:33 -0300

    fixed type model

--- custom_crm_config/models/service_type.py
+++ custom_crm_config/models/service_type.py
@@ -3,6 +3,14 @@ from odoo import models, fields
 class ServiceType(models.Model):
     _name = 'crm.service.type'
     _description = "CRM Services Types"
-    desc_seq = fields.Integer(string='desc sequence')
-    description = fields.Char(string='Description', required=True)
-
+    sequence = fields.Integer(string='sequence')
+    description = fields.Char(string='Description', required=True, translate=True)
+    type = fields.Char(string='Type')
+    dep_id = fields.Selection([
+        ('ESL', 'ESL'),
+        ('Employment', 'Employment'),
+        ('Settlement', 'Settlement'),
+        ('Children & Youth', 'Children & Youth'),
+        ('IT Department', 'IT Department'),
+        ],
+        string='Department')
