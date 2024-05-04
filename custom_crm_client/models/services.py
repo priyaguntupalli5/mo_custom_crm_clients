2fda7561c1068b93f9499882a3922306e3af57a6
morps <morps@mcaf.nb.ca>
2024-05-04 13:17:42 -0700

    [ADD] add service

---
+++ custom_crm_client/models/services.py
@@ -0,0 +1,30 @@
+from odoo import api, models, fields
+
+class Services(models.Model):
+    _name = 'crm.services'
+    _description = "CRM Services"
+   # _inherits = {'res.partner': 'partner_id'}
+
+
+
+    partner_id = fields.Many2one('res.partner', required=True, auto_join=True, index=True)
+    name = fields.Char('Name', required=True)
+    service_id = fields.Char(string='Service ID', index=True, readonly=True, copy=False)
+    description = fields.Text('Description')
+    date = fields.Date(string='Date')
+    responsible = fields.Char('Responsible')
+    tags = fields.Char('Tags')
+    contact = fields.Char('Contact')
+    enddate = fields.Date(string='End date')
+
+    stage_name = fields.Many2one('crm.service.stage',  string='stage_name')
+
+    client_id = fields.Many2one('crm.client',  string='client_id')
+
+
+
+    serviceTypes = fields.Selection([
+    ('Information session', 'Information session'),
+    ('Settlement Plan', 'Settlement Plan'),
+    ('Resume', 'Resume')], string="Service Types")
+
