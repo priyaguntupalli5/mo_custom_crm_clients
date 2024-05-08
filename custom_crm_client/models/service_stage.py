b3a679ca9634a5a0e553684e8214ff6e0325630d
priyaguntupalli5 <guntupallisaipriya@gmail.com>
2024-05-08 00:20:17 -0300

    linked configuration with services

--- custom_crm_client/models/service_stage.py
+++ custom_crm_client/models/service_stage.py
@@ -3,6 +3,8 @@ from odoo import models, fields
 class ServiceStage(models.Model):
     _name = 'crm.service.stage'
     _description = "CRM Services Stages"
+    _rec_name = 'stage_name'
+
     stage_seq = fields.Integer(string='stage sequence')
     stage_name = fields.Char(string='Stage Name', required=True, translate=True)
 
--- custom_crm_client/models/services.py
+++ custom_crm_client/models/services.py
@@ -17,19 +17,19 @@ class Services(models.Model):
     contact = fields.Char('Contact')
     enddate = fields.Date(string='End date')
 
-    stage_name = fields.Many2one('crm.service.stage',  string='stage_name')
+    stage_id = fields.Many2one('crm.service.stage', string='Stage', group_expand='_expand_stage_groups', default=lambda self: self._get_default_stage())
 
-    client_id = fields.Many2one('crm.client',  string='client_id')
+    client_id = fields.Many2one('crm.client', string='client_id')
 
     serviceTypes = fields.Selection([
-    ('Information session', 'Information session'),
-    ('Settlement Plan', 'Settlement Plan'),
-    ('Resume', 'Resume')], string="Service Types")
+        ('Information session', 'Information session'),
+        ('Settlement Plan', 'Settlement Plan'),
+        ('Resume', 'Resume')], string="Service Types")
 
-    state = fields.Selection([
-        ('new','New'),
-        ('in-progress','In Progress'),
-        ('delayed', 'Delayed'),
-        ('done', 'Done')
-    ], default="new", string="Status")
+    def _expand_stage_groups(self, stages, domain, order):
+        return stages.search([], order=order)
+
+    def _get_default_stage(self):
+        default_stage = self.env['crm.service.stage'].search([], limit=1)
+        return default_stage.id if default_stage else False
 
